e6aa7edc161f96f81fcc5eb6a1f4928e2ce779fc
Bilol <madiyarovbilol78@gmail.com>
2025-03-09 14:29:01 +0500

    Deleting user has been finished and bugs are fixed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from '../authantication/roles.guard';
 import { Roles } from '../authantication/roles.decorator';
 import { UpdateUserDto, UserRole, Users } from '../users/users.entity';
+import { identity } from 'rxjs';
 
 @Controller('users')
 @UseGuards(AuthGuard('jwt'))
@@ -16,9 +17,22 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
+  @Roles(UserRole.USER)
   @Put(":id")
-  @Roles(UserRole.ADMIN || UserRole.USER)
-  Update(@Param('id') id: number, @Body() UpdateUserDto: UpdateUserDto) {
-    this.usersService.updateUser(id, UpdateUserDto)
+  Update(@Req() req, @Param() id: number) {
+    return this.usersService.updateUser(req, id)
   }
+
+  @Roles(UserRole.USER || UserRole.ADMIN)
+  @Get(":id")
+  findOne(@Req() req, @Param() id: number) {
+    return this.usersService.findOne(id)
+  }
+
+  @Roles(UserRole.ADMIN || UserRole.USER) 
+  @Delete(":id")
+  deleteUser(@Req() req, @Param() id: number) {
+    return this.usersService.deleteUser(id)
+  }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UpdateUserDto, Users } from './users.entity'; // Ensure correct path
@@ -16,11 +16,23 @@ export class UsersService {
     return await this.userRepository.find();
   }
 
-  async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<Users | null>{
-
-    await this.userRepository.update(id, updateUserDto); 
+  async updateUser(req, id: number): Promise<Users | null>{
+    id = +id['id']
+    await this.userRepository.update(id, req.body); 
     return this.userRepository.findOne({ where: { id } });
   }
 
+  async findOne(id: number): Promise<Users | null>{
+    id = +id['id']
+    return this.userRepository.findOne({ where: { id } })
+  }
 
+  async deleteUser(id: number): Promise<{message: String}>{
+    id = id['id']
+    const user = await this.userRepository.findOne({ where: { id } });
+    if(!user)  throw new Error(`User with ID ${id} not found`);
+
+    await this.userRepository.delete(id)
+    return { message: `User with ID ${id} deleted successfully` };
+  }
 }
